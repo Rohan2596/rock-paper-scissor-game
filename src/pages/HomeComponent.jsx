8477e0615e3bc28c41d2d004feb62803d037f62a
Rohan2596 <rohankadam662@gmail.com>
2022-07-12 20:30:25 +0530

    ADDED: css file and refractec design.

--- src/pages/HomeComponent.jsx
+++ src/pages/HomeComponent.jsx
@@ -1,20 +1,22 @@
 import React from "react";
 import image from "../images/openhand.png"
-
+import "../css/home.css"
 class HomeComponent extends React.Component{
 
     render(){
         return(
-            <div style={{backgroundColor:"#9c27b0",width:"100%",height:"100vh" ,display:"flex",flexDirection:"row",justifyContent:"center",alignItems:"center"}} className="home">
+            <div className="home_container">
                 
-                <div style={{backgroundColor:"#9c27b0",width:"40%",height:"50vh" ,display:"flex",flexDirection:"column",justifyContent:"center",alignItems:"center"}}>
-                <img style={{height:"100vh"}} src={image} alt="open-hand" srcset="" />
+                <div className="home_sub_container">
+                <img className="home_image_container" src={image} alt="open-hand" srcset="" />
 
                 </div>
-                <div style={{backgroundColor:"#9c27b0",width:"40%",height:"50vh" ,display:"flex",flexDirection:"column",justifyContent:"center",alignItems:"center"}} >
-                <span style={{color:"white" ,fontFamily:'Poppins',width:"100%",}}> Created By Rohan Ravindra Kadam </span>
-                <span style={{color:"white" ,fontFamily:'Poppins' ,width:"100%",fontSize:"42px" }}> Rock Paper Scissor </span>
+                <div  className="home_sub_container">
+                <span className="font_style created_by" > Created By Rohan Ravindra Kadam </span>
+                <span className="font_style game_title" > Rock Paper Scissor </span>
+                <div  className="font_style btn_let_play" >Lets Play</div>
                 </div>
+                
                 
                  
 

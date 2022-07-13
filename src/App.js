62ca9e3d97ad3f47327e2f7ed7b5f7714c084b62
Rohan2596 <rohankadam662@gmail.com>
2022-07-13 11:52:23 +0530

    ADDED:Routing and Playground for the game.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import './App.css';
 import HomeComponent from './pages/HomeComponent';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import PlayGroundComponent from './pages/PlayGroundComponent';
 
 
 function App() {
   return (
     <div className="App">
-    <HomeComponent/>
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<HomeComponent/>}/>
+          <Route exact path="/play" element={<PlayGroundComponent/>}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
